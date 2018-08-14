1896a6250ad38788aefc732106a33dc76b5daf6d
Bartosz <bartosz.k.gdz@gmail.com>
2018-08-14 21:20:10 +0000

    poc

--- test/Feree.Validator.Tests/UnitTest1.cs
+++ test/Feree.Validator.Tests/UnitTest1.cs
@@ -3,12 +3,59 @@ using Xunit;
 
 namespace Feree.Validator.Tests
 {
+    public class Dog
+    {
+        public Dog(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            var dogsName = "bob";
 
+            var errors = new Validator<string>()
+                .Must(new NotNull<string, string>(() => dogsName, "dogs name must not be null"))
+                .Must(() => dogsName.Length > 2, "dogs name is too short")
+                .If(() => true)
+                .Validate();
+
+            Assert.Empty(errors);
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            var dogsName = "bb";
+
+            var errors = new Validator<string>()
+                .Must(new NotNull<string, string>(() => dogsName, "dogs name must not be null"))
+                .Must(() => dogsName.Length > 2, "dogs name is too short")
+                .If(() => false)
+                .Validate();
+
+            Assert.Empty(errors);
+        }
+        
+        [Fact]
+        public void Test3()
+        {
+            var dogsName = "bb";
+
+            var dogsNameIsTooShort = "dogs name is too short";
+            var errors = new Validator<string>()
+                .Must(new NotNull<string, string>(() => dogsName, "dogs name must not be null"))
+                .Must(() => dogsName.Length > 2, dogsNameIsTooShort)
+                .If(() => true)
+                .Validate();
+
+            Assert.Contains(dogsNameIsTooShort, errors);
         }
     }
 }
